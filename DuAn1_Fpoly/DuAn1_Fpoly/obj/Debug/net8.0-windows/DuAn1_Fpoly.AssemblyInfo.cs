6279e9ab21356a23c5f6e4442a4c294a471a2288
phupcp9824 <144216475+phupcp9824@users.noreply.github.com>
2024-03-09 11:25:26 +0700

    Add_Control

--- DuAn1_Fpoly/DuAn1_Fpoly/obj/Debug/net8.0-windows/DuAn1_Fpoly.AssemblyInfo.cs
+++ DuAn1_Fpoly/DuAn1_Fpoly/obj/Debug/net8.0-windows/DuAn1_Fpoly.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DuAn1_Fpoly")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+865a6e4aa415111e0c35d41084268b877fc1d425")]
 [assembly: System.Reflection.AssemblyProductAttribute("DuAn1_Fpoly")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DuAn1_Fpoly")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
